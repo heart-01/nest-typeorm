71b393cc1d56679c1bced3f1e058bb2a3ec92c4e
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-06 22:24:02 +0700

    [Feat]: Add .env file and docker-compose.yaml for MySQL configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow('PORT');
+
   setupSwagger(app);
 
   app.setGlobalPrefix('api/v1');
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
